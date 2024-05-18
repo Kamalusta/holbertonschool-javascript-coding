99294dc687a04c5fbef1f8c281b0ba39bd6ba9f2
Kamalusta <kamalusta@gmail.com>
2024-05-18 12:42:17 +0400

    1

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+
+const fs = require('node:fs');
+const args = process.argv
+
+fs.writeFile(args[2], args[3], err => {
+  if (err) {
+    console.error(err);
+  } else {
+    // file written successfully
+  }
+});
