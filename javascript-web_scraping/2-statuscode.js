4bbda1ea90d4e803cb72e46ec9c84f0f3dc6a2fb
Kamalusta <kamalusta@gmail.com>
2024-05-18 14:59:05 +0400

    22

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,9 @@ const request = require('request');
 const args = process.argv;
 
 request(args[2], function (error, response, body) {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
   console.log('code:', response && response.statusCode);
 });
